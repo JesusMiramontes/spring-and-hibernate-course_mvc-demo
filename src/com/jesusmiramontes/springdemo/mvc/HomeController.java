c4e0184b917908952de83a52e17355b1b5990438
Jesús Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2020-09-03 22:19:59 -0500

    set display page

--- src/com/jesusmiramontes/springdemo/mvc/HomeController.java
+++ src/com/jesusmiramontes/springdemo/mvc/HomeController.java
@@ -1,5 +1,15 @@
 package com.jesusmiramontes.springdemo.mvc;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu"; // Return the page name than will look for. This includes the prefix and the sufix previously defined.
+		// Resulting in /WEB-INF/view/main-menu.jsp
+	}
+	
 }
